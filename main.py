7d0d7e89de6f373f4dcebec08e5b08824a4a55af
BrutalRaynor <brutalraynor@gmail.com>
2023-03-16 15:40:21 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from application.salary import calculate_salary
+from application.db.people import get_employees
+from datetime import datetime
+import requests
+
+if __name__ == '__main__':
+    print(calculate_salary)
+    print(get_employees)
+    print(datetime.today)
